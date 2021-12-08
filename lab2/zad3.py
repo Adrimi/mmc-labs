2438cdc89329c7031651767bac8e39c57efad3bc
Adrian Szymanowski <adriano100417@gmail.com>
2021-12-08 13:17:47 +0100

    final form

--- lab2/zad3.py
+++ lab2/zad3.py
@@ -16,7 +16,7 @@ def generate_linear_results(number_of_tries):
     y = uniform(-1, 1)
 
     # Calculate distance from center (0, 0)
-    distance = x * x + y * x
+    distance = x * x + y * y
 
     if distance <= 1:
       points_inside_radix += 1
@@ -24,7 +24,7 @@ def generate_linear_results(number_of_tries):
     N = n + 1
     pi = 4 * points_inside_radix / N
     M = points_inside_radix
-    standard_deviation = V * sqrt(1 / N * M / N * (1 - M / N))
+    standard_deviation = V * sqrt((1 / N) * (M / N) * (1 - (M / N)))
 
     total_numer_of_tries.append(N)
     pi_estimations.append(pi)
@@ -36,13 +36,16 @@ def generate_linear_results(number_of_tries):
 def main():
   iterations = 1000000
 
-  N_arr, pi_arr, est_arr = generate_linear_results(iterations)
+  total_numer_of_tries, pi_estimations, standard_deviations = generate_linear_results(
+      iterations)
 
   with open("zad3.txt", "w") as file:
-    for i in logspace(2, 6, dtype='int'):
+    for i in logspace(1, 6, dtype='int'):
       if i >= iterations:
         return
-      file.write(f'{N_arr[i]} {pi_arr[i]} {est_arr[i]}\n')
+      file.write(
+          f'{total_numer_of_tries[i]} {pi_estimations[i]} {standard_deviations[i]}\n'
+      )
 
 
 if __name__ == '__main__':
