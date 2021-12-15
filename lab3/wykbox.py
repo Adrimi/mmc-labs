3cc046200ed8eb9854639fc0aa3181e12d7db5f1
Adrian Szymanowski <adriano100417@gmail.com>
2021-12-15 21:45:38 +0100

    Zadanie 2

---
+++ lab3/wykbox.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from matplotlib import pyplot as plt
+import numpy as np
+# Biblioteka wspierająca obliczenia naukowe.
+# Importujemy jedynie klasę odpowiedzialną za wyznaczanie
+# różnych wielkości charakteryzujących rozkład normalny.
+from scipy.stats import norm
+
+
+def print(filename):
+  x, y = np.loadtxt(filename, unpack=True)
+  plt.xlim([-4, 4])
+
+  plt.hist(x, bins=100, density=True)
+  plt.hist(y, bins=100, density=True)
+
+  plt.xlabel('Wartość')
+  plt.ylabel('Dystrybuanta')
+  plt.legend(loc='best')
+  plt.show()
+
+
+def main():
+  print('lab3/zad2_box_muller.txt')
+  print('lab3/zad2_marsaglii_braya.txt')
+
+
+if __name__ == '__main__':
+  main()
--- lab3/zad2.cpp
+++ lab3/zad2.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <random>
 #include <math.h>
 
 #define PI 3.141592653589793
 
-double power(double base, double exponent)
+double superpower(double base, double exponent)
 {
   if (base < 0)
   {
@@ -18,31 +19,69 @@ double power(double base, double exponent)
 
 double standart_twodim_generator(double x, double y)
 {
-  return (1 / (2 * PI)) * exp((-1) * (power(x, 2) + power(y, 2)) / 2);
+  return (1 / (2 * PI)) * exp((-1) * (superpower(x, 2) + superpower(y, 2)) / 2);
 }
 
-double uniform_random_number(double min, double max)
+double randomix_2000(double min, double max)
 {
-  std::uniform_real_distribution<double> unif(min, max);
-  std::default_random_engine re;
-  return (double)unif(re);
+  std::uniform_real_distribution<float> uniform;
+  std::random_device rd;
+  std::mt19937 randomSource(rd());
+  return (double)uniform(randomSource);
 }
 
-double box_mueller(double min, double max)
+std::pair<double, double> marsaglii_braya()
 {
-  double u1 = uniform_random_number(min, max);
-  double u2 = uniform_random_number(min, max);
+  double u, v;
+  double b = 0;
+
+  do
+  {
+    u = randomix_2000(0, 1);
+    v = randomix_2000(0, 1);
+    b = u * u + v * v;
+  } while (b >= 1);
+
+  double z = sqrt(-2. * log(b) / b);
+
+  return std::pair<double, double>(u * z, v * z);
+}
+
+std::pair<double, double> box_mueller()
+{
+  double u1 = randomix_2000(0, 1);
+  double u2 = randomix_2000(0, 1);
 
   double x = (sqrt((-2) * log(u1)) * cos(2 * PI * u2));
   double y = (sqrt((-2) * log(u1)) * sin(2 * PI * u2));
 
-  return standart_twodim_generator(x, y);
+  return std::pair<double, double>(x, y);
 }
 
 int main()
 {
-  double random_number_2 = log(-1.0);
-  std::cout << random_number_2 << std::endl;
+  std::ofstream ofs;
+  int iterations = 100000;
+
+  ofs.open("zad2_box_muller.txt");
+
+  for (int i = 0; i < iterations; i++)
+  {
+    auto point = box_mueller();
+    ofs << point.first << " " << point.second << std::endl;
+  }
+
+  ofs.close();
+
+  ofs.open("zad2_marsaglii_braya.txt");
+
+  for (int i = 0; i < iterations; i++)
+  {
+    auto point = marsaglii_braya();
+    ofs << point.first << " " << point.second << std::endl;
+  }
+
+  ofs.close();
 
   return 0;
 }
